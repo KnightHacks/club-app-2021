9957c20f178cb0e21b8d63ee6c99abbc0f9a8fe7
beedude <suneettipirneni@icloud.com>
2021-02-28 17:25:23 -0500

    Get basic layout

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/LoginContainer.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -10,10 +11,10 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Test"),
+        title: Text("Login"),
       ),
       body: SafeArea(
-        child: Column()
+        child: LoginContainer()
       ),
     );
   }
