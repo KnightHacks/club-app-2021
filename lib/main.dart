3aa4721893859a1303ad3c293f593ea87bbb24c7
beedude <suneettipirneni@icloud.com>
2021-03-01 22:39:18 -0500

    fix routes

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(),
       initialRoute: "/",
       routes: {
-        "/" : (context) => FAQ(),
+        "/" : (context) => Login(),
         "/Login" : (context) => Login(),
         "/Register1" : (context) => Register1(),
         "/Register2" : (context) => Register2(),
