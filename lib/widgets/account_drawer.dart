b9ab71ac331b258c9f0beba159a7fe6b88d8e92a
beedude <suneettipirneni@icloud.com>
2021-03-03 22:01:59 -0500

    Add Stubs for new code.

--- lib/widgets/account_drawer.dart
+++ lib/widgets/account_drawer.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AccountDrawer extends StatelessWidget {
+
+  final _auth = FirebaseAuth.instance;
+  FirebaseUser _user;
+
+  void logout(FirebaseUser user) {
+    _auth.signOut();
+
+    // Move back to the Login page.
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
+    try {
+      _auth.currentUser()
+          .then((user) => _user);
+    } catch(e) {
+      print(e);
+    }
+
     return Drawer(
       child: ListView(
         children: <Widget>[
           DrawerHeader(
-            child: Text(
-              "Header",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
+            child: Image.asset("assets/knightHacksLogoGold.png"),
             decoration: BoxDecoration(
               color: Colors.deepPurple
             ),
           ),
           ListTile(
-            title: Text(
-                "Suneet Tipirneni",
+            title: Text(_user?.displayName ?? "Display Name",
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 24
@@ -33,8 +46,10 @@ class AccountDrawer extends StatelessWidget {
             title: Text("email@email.com"),
             leading: Icon(Icons.email),
           ),
-          FlatButton(
-              onPressed: () {},
+          FlatButton( // Left Justify
+              onPressed: () {
+                // Log the user out.
+              },
               child: Text("Logout", textAlign: TextAlign.left,)
           )
         ],
