d8444c0a5e91917c5378e1d00d2f2cbee92541b8
unknown <arjunp@knights.ucf.edu>
2021-03-07 16:55:16 -0500

    list events and [attempt] to add to calendar

--- lib/model/Event.dart
+++ lib/model/Event.dart
@@ -1,33 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:ical/serializer.dart';
+
 class Event {
   String title;
   String description;
-  String time;
   String location;
-  String date;
-  String month;
+  DateTime dateTime;
   String presenter;
 
   Event({
     this.title,
     this.description,
-    this.time,
     this.location,
-    this.date,
-    this.month,
+    this.dateTime,
     this.presenter
   });
 
   Event.from(Map<String, String> map) {
     title = map["title"];
     description = map["description"];
-    time = map["time"];
     location = map["location"];
-    date = map["date"];
-    month = map["month"];
+    dateTime = DateTime.parse(map["dateTime"]);
     presenter = map["presenter"];
   }
 
   String toString() {
     return title+" "+presenter;
+  }
+
+  ICalendar addToCal() {
+    ICalendar ical = ICalendar();
+    ical.addElement(
+      IEvent(
+        start: this.dateTime,
+        summary: this.title,
+        description: this.description
+      )
+    );
+    return ical;
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:club_app_2021/widgets/title_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:club_app_2021/model/Event.dart';
+import 'package:share/share.dart';
 
 class Home extends StatefulWidget {
   static const String id = "Home";
@@ -15,50 +16,40 @@ class _HomeState extends State<Home> {
       "title": "Alexa Stock Ticker",
       "description":
       "Learn how to make an Alexa skill with Knight Hacks! No coding experience is required. We will be making an Alexa skill that gets stock prices so you can ask your device what your favorite company is trading at. Along the way, we'll demonstrate some basic programming concepts for participants who are new to programming. We'll also be giving away an Amazon Echo device to a lucky workshop participant!",
-      "time": "7:30 p.m.",
+      "dateTime": "2021-03-07T20:47:34+00:00",
       "location": "Zoom",
-      "date": "25",
-      "month": "Feb",
       "presenter": "Chris Feltner",
     },
     {
       "title": "What's the Point of Pointers?",
       "description":
       "Memory allocation is one of the more difficult parts of CS1, so this week we are going to do a review of the topic to help you succeed in your class! Knight Hacks President Chris Feltner will go over what pointers are, how to use them, and how to allocate and free memory. We'll be making a meal ordering system for the terminal in C to demonstrate these techniques.",
-      "time": "7:30 p.m.",
+      "dateTime": "2021-03-07",
       "location": "Zoom",
-      "date": "02",
-      "month": "Mar",
       "presenter": "Chris Feltner",
     },
     {
       "title": "Vim and Vigor",
       "description":
       "Interested in a text editor included with nearly every Linux server and installation of MacOS? Ever typed `git commit` and gotten stuck in a strange program where the keys do seemingly random things? Curious about this `vim` thing you've seen Rob typing at `Hello, World!` ? Whatever your reason, this workshop is for you! We will learn the legendary Vim text editor, from basics like how to exit, to the powerful keybindings that have made it a popular choice for decades. No experience required!",
-      "time": "7:30 p.m.",
+      "dateTime": "2021-03-07",
       "location": "Zoom",
-      "date": "04",
-      "month": "Mar",
       "presenter": "Robert Boyd",
     },
     {
       "title": "Career Development Panel",
       "description":
       "Join us for a panel with former students who have graduated and now work full time! You can listen and ask questions about their path into their current career and what they might have done differently. This panel is a great opportunity to learn about the reality of the job market and what you need to do to be successful!",
-      "time": "7:30 p.m.",
+      "dateTime": "2021-03-07",
       "location": "Zoom",
-      "date": "11",
-      "month": "Mar",
-      "presenter": "",
+      "presenter": "Someone",
     },
     {
       "title": "Rust",
       "description":
       'Rust has been the undisputed "most loved" programming language on the Stack Overflow Developer Survey for several years. Why? Come to this workshop and find out! Anthony will be returning to teach us the basics of Rust\'s unique memory management model and answer any questions about this innovative and promising new programming language.',
-      "time": "7:30 p.m.",
+      "dateTime": "2021-03-07",
       "location": "Zoom",
-      "date": "18",
-      "month": "Mar",
       "presenter": "Anthony Hevia",
     },
   ];
@@ -78,9 +69,15 @@ class _HomeState extends State<Home> {
       appBar: titleBar,
       body: SafeArea(
         child: ListView(
-          children: events.map((e) => ListTile(
-            title: Text(e.title),
-            subtitle: Text(e.description),
+          children: events.map((e) => Column(
+            children: <Widget>[
+              Text(e.title),
+              Text(e.description),
+              FlatButton(onPressed: (){
+                Share.share("text");
+                //Share.shareFiles([e.addToCal().serialize()]);
+              }, child: Text('Click me'))
+            ],
           )).toList(),
         ),
       ),
