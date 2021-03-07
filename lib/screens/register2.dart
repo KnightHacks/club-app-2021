cf2f23caaa21c3301ebe55a65cfb889362299151
Elbert <etse168@gmail.com>
2021-03-06 22:18:22 -0500

    Fixed Arjun's merge

--- lib/screens/register2.dart
+++ lib/screens/register2.dart
@@ -35,97 +35,99 @@ class _Register2State extends State<Register2> {
     final inputBorder =
         OutlineInputBorder(borderRadius: BorderRadius.circular(30));
     return Scaffold(
-      appBar: titleBar,
-
-      body: SafeArea(
+        appBar: titleBar,
+        body: SafeArea(
           child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // contains knighthacks logo
-            // Image.asset(
-            //   "assets/sword.png",
-            //   width: 50,
-            // ),
-            // SizedBox(height: 20),
-            TextFormField(
-              //contains email
-              decoration:
-                  InputDecoration(labelText: "Email", border: inputBorder),
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            SizedBox(height: 10),
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // contains knighthacks logo
+                  // Image.asset(
+                  //   "assets/sword.png",
+                  //   width: 50,
+                  // ),
+                  // SizedBox(height: 20),
+                  TextFormField(
+                    //contains email
+                    decoration: InputDecoration(
+                        labelText: "Email", border: inputBorder),
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                  ),
+                  SizedBox(height: 10),
 
-            // contains password
-            TextFormField(
-              decoration:
-                  InputDecoration(labelText: "Password", border: inputBorder),
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            SizedBox(height: 10),
-            // contains confirmation of password
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: "Confirm Password", border: inputBorder),
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                confirmPassword = value;
-              },
-            ),
-            SizedBox(height: 20),
-            // Creates button for Registration
-            RoundedButton(
-              child: Text("Register"),
-              buttonColor: Color(0xFFb7517c),
-              onPressed: () async {
-                if (confirmPassword == password) {
-                  AuthResult res = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  FirebaseUser user = res.user;
-                  _firestore.collection("user").add({
-                    'fullName': _user.fullName,
-                    'uid': user.uid,
-                    'street': _user.street,
-                    'apartment': _user.apartment,
-                    'state': _user.state,
-                    'zip': _user.zip,
-                    'shirtSize': _user.shirtSize
-                  });
+                  // contains password
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Password", border: inputBorder),
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  // contains confirmation of password
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Confirm Password", border: inputBorder),
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      confirmPassword = value;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  // Creates button for Registration
+                  RoundedButton(
+                    child: Text("Register"),
+                    buttonColor: Color(0xFFb7517c),
+                    onPressed: () async {
+                      if (confirmPassword == password) {
+                        AuthResult res =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        FirebaseUser user = res.user;
+                        _firestore.collection("user").add({
+                          'fullName': _user.fullName,
+                          'uid': user.uid,
+                          'street': _user.street,
+                          'apartment': _user.apartment,
+                          'state': _user.state,
+                          'zip': _user.zip,
+                          'shirtSize': _user.shirtSize
+                        });
 
-                  try{
-                    await user.sendEmailVerification();
-                  } catch (e) {
-                    print(
-                        "Something went wrong while sending your email verification.");
-                    print(e.message);
-                  }
+                        try {
+                          await user.sendEmailVerification();
+                        } catch (e) {
+                          print(
+                              "Something went wrong while sending your email verification.");
+                          print(e.message);
+                        }
 
-                  Navigator.popAndPushNamed(context, Confirm.id);
-              }
-            },
-          ),
-          SizedBox(height: 20),
-          // Creates button for Going Back to Register 1
-          RoundedButton(
-            child: Text("Go Back"),
-            buttonColor: Colors.amber,
-            onPressed: () => Navigator.popAndPushNamed(context, Register1.id);
-          ),
-          SizedBox(height: 20),
-        ],
-      )
-      ),
-    );
-  }
+                        Navigator.popAndPushNamed(context, Confirm.id);
+                      }
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  // Creates button for Going Back to Register 1
+                  RoundedButton(
+                    child: Text("Go Back"),
+                    buttonColor: Colors.amber,
+                    onPressed: () =>
+                        Navigator.popAndPushNamed(context, Register1.id),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              )
+            ),
+        )
+      );
+   }
 }
