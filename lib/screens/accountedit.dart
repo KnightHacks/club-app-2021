a43b5cfa0bf425775fb4fa2998c0e9d26444160e
Elbert <etse168@gmail.com>
2021-03-16 16:59:42 -0400

    Passed KnightHackUser to accountedit from account drawer

--- lib/screens/accountedit.dart
+++ lib/screens/accountedit.dart
@@ -1,49 +1,29 @@
+import 'package:club_app_2021/model/KnightHacksUser.dart';
 import 'package:club_app_2021/widgets/rounded_input.dart';
 import 'package:club_app_2021/widgets/title_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:club_app_2021/model/Prop.dart';
 
 class AccountEdit extends StatefulWidget {
   static const String id = "AccountEdit";
-  
+
   @override
   _AccountEditState createState() => _AccountEditState();
 }
 
-/*
-Map <String, Dynamic> data;
-String id = user.uid;
-        final _firestore = Firestore.instance;
-        KnightHackUser khUser;
-
-        await _firestore
-            .collection('user')
-            .where('uid', isEqualTo: id)
-            .getDocuments()
-            .then((value) {
-          // always returns an array of documents
-          // casting it as a Map
-          data = Map.from(value.documents[0].data);
-        });
-
-        khUser = new KnightHackUser(
-          uid: data["uid"].toString(),
-          fullName: data["fullName"].toString(),
-          street: data["street"].toString(),
-          apartment: data["apartment"].toString(),
-          city: data["city"].toString(),
-          state: data["state"].toString(),
-          zip: data["zip"].toString(),
-          shirtSize: data["shirtSize"].toString(),
-        );
-
-  use this to hit db
-*/
-
-
-
 class _AccountEditState extends State<AccountEdit> {
+
   @override
   Widget build(BuildContext context) {
+
+    try{
+      final Prop prop = ModalRoute.of(context).settings.arguments;
+      KnightHackUser khUser = prop.knightHackUser;
+      
+    } catch(e){
+      print(e.toString());
+    }
+
     return Scaffold(
       appBar: titleBar,
       body: SafeArea(
@@ -51,44 +31,44 @@ class _AccountEditState extends State<AccountEdit> {
           margin: EdgeInsets.symmetric(horizontal: 50),
           child: SingleChildScrollView(
             child: Column(
-            children: [
-              SizedBox(height: 10),
-              RoundedTextInput(
-                labelText: "Full Name",
-                autocorrect: false,
-              ),
-              SizedBox(height: 10),
-              RoundedTextInput(
-                labelText: "Street Address",
-                autocorrect: false,
-              ),
-              SizedBox(height: 10),
-              RoundedTextInput(
-                labelText: "Apartment (Optional)",
-                autocorrect: false,
-              ),
-              SizedBox(height: 10),
-              RoundedTextInput(
-                labelText: "Zipcode",
-                autocorrect: false,
-              ),
-              SizedBox(height: 10),
-              RoundedTextInput(
-                labelText: "City",
-                autocorrect: false,
-              ),
-              SizedBox(height: 10),
-              RoundedTextInput(
-                labelText: "State",
-                autocorrect: false,
-              ),
-              SizedBox(height: 10),
-              RoundedTextInput(
-                labelText: "T-shirt Size (Unisex)",
-                autocorrect: false,
-              ),
-            ],
-          ),
+              children: [
+                SizedBox(height: 10),
+                RoundedTextInput(
+                  labelText: "Full Name",
+                  autocorrect: false,
+                ),
+                SizedBox(height: 10),
+                RoundedTextInput(
+                  labelText: "Street Address",
+                  autocorrect: false,
+                ),
+                SizedBox(height: 10),
+                RoundedTextInput(
+                  labelText: "Apartment (Optional)",
+                  autocorrect: false,
+                ),
+                SizedBox(height: 10),
+                RoundedTextInput(
+                  labelText: "Zipcode",
+                  autocorrect: false,
+                ),
+                SizedBox(height: 10),
+                RoundedTextInput(
+                  labelText: "City",
+                  autocorrect: false,
+                ),
+                SizedBox(height: 10),
+                RoundedTextInput(
+                  labelText: "State",
+                  autocorrect: false,
+                ),
+                SizedBox(height: 10),
+                RoundedTextInput(
+                  labelText: "T-shirt Size (Unisex)",
+                  autocorrect: false,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/account_drawer.dart
+++ lib/widgets/account_drawer.dart
@@ -1,7 +1,10 @@
+import 'package:club_app_2021/model/Prop.dart';
 import 'package:club_app_2021/screens/accountedit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:club_app_2021/model/KnightHacksUser.dart';
 
 class AccountDrawer extends StatefulWidget {  
   @override
@@ -12,6 +15,46 @@ class _AccountDrawerState extends State<AccountDrawer> {
 
   final _auth = FirebaseAuth.instance;
   FirebaseUser _user;
+
+  KnightHackUser khUser;
+  Map<String, dynamic> data;
+
+  void _goToEdit(BuildContext context) async {
+    try{
+      FirebaseUser user = await FirebaseAuth.instance.currentUser();
+
+      String id = user.uid;
+      final _firestore = Firestore.instance;
+
+      await _firestore
+          .collection('user')
+          .where('uid', isEqualTo: id)
+          .getDocuments()
+          .then((value) {
+        // always returns an array of documents
+        // casting it as a Map
+        data = value.documents[0].data;
+
+        khUser = new KnightHackUser(
+          uid: data["uid"].toString(),
+          fullName: data["fullName"].toString(),
+          street: data["street"].toString(),
+          apartment: data["apartment"].toString(),
+          city: data["city"].toString(),
+          state: data["state"].toString(),
+          zip: data["zip"].toString(),
+          shirtSize: data["shirtSize"].toString(),
+        );
+      });
+
+      // print(khUser.toString());
+      Navigator.pushNamed(context, AccountEdit.id, arguments: Prop(khUser));
+    } catch(e){
+
+      print(e.toString());
+    }
+  }
+
 
   void logout(BuildContext context) {
     _auth.signOut().then((value) => {
@@ -47,7 +90,7 @@ class _AccountDrawerState extends State<AccountDrawer> {
             leading: Icon(Icons.email),
           ),
           FlatButton(
-            onPressed: () => Navigator.pushNamed(context, AccountEdit.id),
+            onPressed: () => _goToEdit(context),
             child: Text("Edit"),
             color: Colors.amber,
           ),
