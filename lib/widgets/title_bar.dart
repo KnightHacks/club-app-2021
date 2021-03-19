b29fd5423af34f8721fc8f3f6965abeee180625c
Elbert <etse168@gmail.com>
2021-03-19 08:44:03 -0400

    Removed elevation

--- lib/widgets/title_bar.dart
+++ lib/widgets/title_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 AppBar titleBar = AppBar(
         centerTitle: true,
-        elevation: 0,
         title: Image.asset(
           "assets/knightHacksLogoGold.png",
           width: 150,
