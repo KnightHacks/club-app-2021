b5f63d10f22b96fc129c40e2510040357b0d7a74
Elbert <etse168@gmail.com>
2021-03-25 18:36:57 -0400

    Stylized register 1

--- lib/screens/register1.dart
+++ lib/screens/register1.dart
@@ -42,73 +42,87 @@ class _Register1State extends State<Register1> {
         child: SingleChildScrollView(
           child: Form(
             key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RoundedTextInput(
-                  validator: _validate,
-                  labelText: "Full Name",
-                  controller: _fullNameController,
-                ),
-                RoundedTextInput(
-                  validator: _validate,
-                  labelText: "Street",
-                  controller: _streetController,
-                ),
-                RoundedTextInput(
-                  labelText: "Apartment",
-                  controller: _aptController,
-                ),
-                RoundedTextInput(
-                  validator: _validate,
-                  labelText: "City",
-                  controller: _cityController,
-                ),
-                RoundedTextInput(
-                  validator: _validate,
-                  labelText: "State",
-                  controller: _stateController,
-                ),
-                RoundedTextInput(
-                  validator: _validate,
-                  labelText: "ZIP",
-                  controller: _zipController,
-                ),
-                RoundedTextInput(
-                  validator: _validate,
-                  labelText: "Shirt",
-                  controller: _shirtSizeController,
-                ),
-                RoundedButton(
-                  child: Text("Continue"),
-                  onPressed: () {
-                    // Validate inputs
-                    _formKey.currentState.validate();
-                    _user = KnightHackUser(
-                      fullName: _fullNameController.text,
-                      street: _streetController.text,
-                      apartment: _aptController.text,
-                      city: _cityController.text,
-                      state: _stateController.text,
-                      zip: _zipController.text,
-                      shirtSize: _shirtSizeController.text
-                    );
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  RoundedTextInput(
+                    validator: _validate,
+                    labelText: "Full Name",
+                    controller: _fullNameController,
+                  ),
+                  SizedBox(height: 20),
+                  RoundedTextInput(
+                    validator: _validate,
+                    labelText: "Street",
+                    controller: _streetController,
+                  ),
+                  SizedBox(height: 20),
+                  RoundedTextInput(
+                    labelText: "Apartment",
+                    controller: _aptController,
+                  ),
+                  SizedBox(height: 20),
+                  RoundedTextInput(
+                    validator: _validate,
+                    labelText: "City",
+                    controller: _cityController,
+                  ),
+                  SizedBox(height: 20),
+                  RoundedTextInput(
+                    validator: _validate,
+                    labelText: "State",
+                    controller: _stateController,
+                  ),
+                  SizedBox(height: 20),
+                  RoundedTextInput(
+                    validator: _validate,
+                    labelText: "ZIP",
+                    controller: _zipController,
+                  ),
+                  SizedBox(height: 20),
+                  RoundedTextInput(
+                    validator: _validate,
+                    labelText: "Shirt",
+                    controller: _shirtSizeController,
+                  ),
+                  SizedBox(height: 20),
+                  RoundedButton(
+                    child: Text("Continue"),
+                    onPressed: () {
+                      // Validate inputs
+                      _formKey.currentState.validate();
+                      _user = KnightHackUser(
+                        fullName: _fullNameController.text,
+                        street: _streetController.text,
+                        apartment: _aptController.text,
+                        city: _cityController.text,
+                        state: _stateController.text,
+                        zip: _zipController.text,
+                        shirtSize: _shirtSizeController.text
+                      );
 
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Register2(
-                          user: _user,
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Register2(
+                            user: _user,
+                          )
                         )
-                      )
-                    );
-                  },
-                ),
-                RoundedButton(
-                  child: Text("Go Back"),
-                  onPressed: () => Navigator.pop(context),
-                )
-              ],
+                      );
+                    },
+                    buttonColor: Colors.amber,
+                  ),
+                  SizedBox(height: 10),
+                  RoundedButton(
+                    child: Text("Go Back"),
+                    onPressed: () => Navigator.pop(context),
+                    buttonColor: Colors.pinkAccent
+                  )
+                ],
+              ),
             ),
           ),
         )
